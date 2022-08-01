d31ad0a0e9b4c61786e8cef7a0a8f13c8fa6530b
sean <me@me.me>
2022-08-01 16:14:23 -0700

    initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "sean-env"
+
+    workspaces {
+      name = "file_template-workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "template_file" "my_json_template" {
+  template = file("my-json-file.json")
+  vars = {
+    ROOT_VEGETABLE = var.ROOT_VEGETABLE
+  }
+}
